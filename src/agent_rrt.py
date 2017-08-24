1a4efc08c411dece8469a2f338e1fe905077d41b
Christen Ford <christen0ford@gmail.com>
2017-08-24 09:47:41 -0400

    Get basic 3D RRT working (Still need to implement RT stuff)

--- src/agent_rrt.py
+++ src/agent_rrt.py
@@ -2,13 +2,6 @@ from __future__ import division
 import math, time, random
 
 
-class Math:
-
-    @staticmethod
-    def dist(p1, p2):
-        return math.sqrt((p1[0]-p2[0])*(p1[0]-p2[0])+(p1[1]-p2[1])*(p1[1]-p2[1])+(p1[2]-p2[2])*(p1[2]-p2[2]))
-
-
 class Timer:
 
     def __init__(self):
@@ -19,13 +12,13 @@ class Timer:
 
     def reset(self):
         self.current_time = time.time()
-        
+
 
 class RRTAgent:
 
-    def __init__(self, start=(0, 0, 0), goal=(50, 5, 50),
-                 xdims=(-50, 50), ydims=(-5, 5), zdims=(-50, 50),
-                 alpha=0.1, beta=3.25, epsilon=7.0,
+    def __init__(self, start=(0, 0, 0), goal=(15, 0, 15),
+                 xdims=(-25, 25), ydims=(-10, 10), zdims=(-25, 25),
+                 alpha=0.1, beta=1.25, epsilon=7.0,
                  xrad=2.0, yrad=2.0, zrad=2.0,
                  move_time=0.5, path_time=0.5):
         if alpha < 0 or alpha >= 1: raise ValueError
@@ -64,6 +57,26 @@ class RRTAgent:
     def get_zmax(self):
         return max(self.dimensions[2])
 
+    @staticmethod
+    def dist(p1, p2):
+        return math.sqrt((p1[0]-p2[0])*(p1[0]-p2[0])+(p1[1]-p2[1])*(p1[1]-p2[1])+(p1[2]-p2[2])*(p1[2]-p2[2]))
+
+    def get_path(self, node):
+        path = []
+
+        while node.parent:
+            path.append(node.pos)
+            node = node.parent
+
+        return path
+
+    def is_goal(self, p):
+        x, y, z = p
+        if x >= self.goal[0]-.5 and x <= self.goal[0]+.5:
+            if y >= self.goal[1]-.5 and y <= self.goal[1]+.5:
+                if z >= self.goal[2]-.5 and z <= self.goal[2]+.5:
+                    return True
+        return False
 
     def in_bounds(self, p):
         x, y, z = p
@@ -81,17 +94,17 @@ class RRTAgent:
                 random.uniform(-self.radii[2], self.radii[2]))
 
     def line_to(self, p1, p2):
-        if Math.dist(p1, p2) < self.epsilon:
+        if self.dist(p1, p2) < self.epsilon:
             return p2
         else:
             theta = math.atan2(p2[1]-p1[1], p2[0]-p1[0])
             mx, my = math.cos(theta), math.sin(theta)
             return p1[0] + mx * self.epsilon, p1[1] + my * self.epsilon, p2[1] + (my/mx) * self.epsilon
 
-    def sample(self):
+    def sample(self, p1, p2):
         p = random.random()
         if p <= 1-self.alpha:
-            return self.line_to()
+            return self.line_to(p1, p2)
         # TODO: This needs a los check added to it
         elif p <= (1-self.alpha)/self.beta:
             return self.uniform()
@@ -102,3 +115,36 @@ class RRTAgent:
         return (random.uniform(self.get_xmin(), self.get_xmax()),
                 random.uniform(self.get_ymin(), self.get_ymax()),
                 random.uniform(self.get_zmin(), self.get_zmax()))
+
+class Node:
+
+    def __init__(self, pos, parent=None):
+        self.pos = pos
+        self.parent = parent
+
+def test3d():
+
+    agent = RRTAgent()
+    nodes = [Node(agent.position)]
+    found = False
+
+    for i in range(5000):
+        rand = agent.uniform()
+        nn = nodes[0]
+        for p in nodes:
+            if RRTAgent.dist(p.pos, rand) < RRTAgent.dist(nn.pos, rand):
+                nn = p
+        nodes.append(Node(agent.sample(nn.pos, rand), nn))
+        if agent.is_goal(nodes[-1].pos):
+            found = True
+            path = agent.get_path(nodes[-1])
+            print "Path is: "
+            for i in range(len(path)):
+                print "{0}.) {1}".format(i+1, path[i])
+            break
+
+    if not found:
+        print "There was no path found :("
+
+if __name__ == '__main__':
+    test3d()
